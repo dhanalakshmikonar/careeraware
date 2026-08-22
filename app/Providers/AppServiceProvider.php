5a78abf3dad73420348cb25874dea41af8effa92
dhanalakshmikonar <konardhanalakshmi@gmail.com>
2026-08-22 23:19:50 +0530

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,7 +20,11 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        Paginator::useBootstrapFive();
+{
+    Paginator::useBootstrapFive();
+
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
     }
+}
 }
